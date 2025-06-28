41e73ffa51cfe6505a0ede59fc25e34b6155247d
0Migrant0 <towls320@gmail.com>
2025-06-28 03:00:59 +0300

    Refactor styles for add clinic page and improve layout responsiveness

--- booking_system/components/add_clinic.php
+++ booking_system/components/add_clinic.php
@@ -143,7 +143,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>إضافة عيادة</title>
-    <link rel="stylesheet" href="../styles/styles.css"> <!-- تأكد من أن اسم الملف صحيح -->
+    <link rel="stylesheet" href="../styles/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
@@ -225,8 +225,7 @@ try {
 
 
         <div class="add-clinic-container" id="add-clinic-container">
-            <h2><i class="fas fa-plus-circle"></i> إضافة عيادة</h2>
-<br>
+            <h2 class="add-clinic-container-title"> إضافة عيادة</h2>
             <form action="add_clinic.php" method="post">
                 <!-- Governorate -->
                 <label for="governorate">المحافظة</label>
